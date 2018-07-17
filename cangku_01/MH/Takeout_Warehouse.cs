0958ccb509cd7cf18649c42a9d1a8c61ae3c57de
mahua <admin@DESKTOP-OSRP7ML>
2018-07-17 10:40:03 +0800

    出入库实体类2018-7-17

--- cangku_01/MH/Takeout_Warehouse.cs
+++ cangku_01/MH/Takeout_Warehouse.cs
@@ -10,13 +10,14 @@ using System.Windows.Forms;
 
 namespace cangku_01.MH
 {
+    //仪器出库页面
     public partial class Takeout_Warehouse : Form
     {
         public Takeout_Warehouse()
         {
             InitializeComponent();
         }
-
+        //调用方法固定页面
         private void Takeout_Warehouse_Load(object sender, EventArgs e)
         {
             Take_Window_style win_sty = new Take_Window_style();
