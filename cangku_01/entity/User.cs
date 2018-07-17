a3cb9874b722580cfbfc06190a4bfb9f64b7c6fc
sunqiangqiang <Mr_sunqiangqiang@hotmail.com>
2018-07-17 16:27:33 +0800

    员工录入2018-07-17

--- cangku_01/entity/User.cs
+++ cangku_01/entity/User.cs
@@ -8,22 +8,23 @@ namespace cangku_01.entity
 {
     class User
     {
-        private String id;
+        private int id;
         private String name;
         private String sex;
         private String tel;
         private String temp;
         private String job;
-        private String salary;
-        private String time;
+        private double salary;
+        private DateTime time;
 
-        public String Id { get => id; set => id = value; }
+        public int Id { get => id; set => id = value; }
         public string Name { get => name; set => name = value; }
         public string Sex { get => sex; set => sex = value; }
         public string Tel { get => tel; set => tel = value; }
         public string Temp { get => temp; set => temp = value; }
         public string Job { get => job; set => job = value; }
-        public String Salary { get => salary; set => salary = value; }
-        public String Time { get => time; set => time = value; }
+        public double Salary { get => salary; set => salary = value; }
+
+        public DateTime Time { get => time; set => time = value; }
     }
 }
