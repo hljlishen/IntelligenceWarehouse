c18e0b6bcb65be7d3cdb514a8b4583a6bfc64eaf
lixin <lixin0209@hotmail.com>
2018-07-26 08:43:17 +0800

    仪器管理 合并添加修改界面

--- cangku_01/LX/AddOrModifyInstrument.designer.cs
+++ cangku_01/LX/AddOrModifyInstrument.designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace cangku_01.LX
 {
-    partial class Updata_instrument
+    partial class AddOrModifyInstrument
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.time_lastCheckTimes = new System.Windows.Forms.DateTimePicker();
+            this.tb_instrumentNumber = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
             this.time_productionDate = new System.Windows.Forms.DateTimePicker();
-            this.label13 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.bt_no = new System.Windows.Forms.Button();
+            this.bt_yes = new System.Windows.Forms.Button();
             this.tb_manager = new System.Windows.Forms.TextBox();
             this.tb_checkCycle = new System.Windows.Forms.TextBox();
             this.tb_isInWareHouse = new System.Windows.Forms.TextBox();
@@ -52,247 +55,242 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.tb_instrumentNumber = new System.Windows.Forms.TextBox();
-            this.time_lastCheckTimes = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
+            // 
+            // time_lastCheckTimes
+            // 
+            this.time_lastCheckTimes.Location = new System.Drawing.Point(529, 226);
+            this.time_lastCheckTimes.Name = "time_lastCheckTimes";
+            this.time_lastCheckTimes.Size = new System.Drawing.Size(128, 21);
+            this.time_lastCheckTimes.TabIndex = 57;
+            // 
+            // tb_instrumentNumber
+            // 
+            this.tb_instrumentNumber.Location = new System.Drawing.Point(529, 283);
+            this.tb_instrumentNumber.Name = "tb_instrumentNumber";
+            this.tb_instrumentNumber.Size = new System.Drawing.Size(125, 21);
+            this.tb_instrumentNumber.TabIndex = 56;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(433, 292);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(65, 12);
+            this.label10.TabIndex = 55;
+            this.label10.Text = "当前数量：";
             // 
             // time_productionDate
             // 
-            this.time_productionDate.Location = new System.Drawing.Point(160, 337);
+            this.time_productionDate.Location = new System.Drawing.Point(220, 340);
             this.time_productionDate.Name = "time_productionDate";
             this.time_productionDate.Size = new System.Drawing.Size(125, 21);
             this.time_productionDate.TabIndex = 54;
             this.time_productionDate.Value = new System.DateTime(2018, 7, 19, 11, 11, 56, 0);
             // 
-            // label13
+            // title
             // 
-            this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("宋体", 15F);
-            this.label13.Location = new System.Drawing.Point(254, 31);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(169, 20);
-            this.label13.TabIndex = 53;
-            this.label13.Text = "修改仪器基本信息";
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("宋体", 15F);
+            this.title.Location = new System.Drawing.Point(316, 37);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(0, 20);
+            this.title.TabIndex = 53;
             // 
-            // button2
+            // bt_no
             // 
-            this.button2.Location = new System.Drawing.Point(412, 387);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 52;
-            this.button2.Text = "取消";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.bt_no.Location = new System.Drawing.Point(472, 390);
+            this.bt_no.Name = "bt_no";
+            this.bt_no.Size = new System.Drawing.Size(75, 23);
+            this.bt_no.TabIndex = 52;
+            this.bt_no.Text = "取消";
+            this.bt_no.UseVisualStyleBackColor = true;
+            this.bt_no.Click += new System.EventHandler(this.bt_no_Click_1);
             // 
-            // button1
+            // bt_yes
             // 
-            this.button1.Location = new System.Drawing.Point(202, 387);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 51;
-            this.button1.Text = "确认";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.bt_yes.Location = new System.Drawing.Point(262, 390);
+            this.bt_yes.Name = "bt_yes";
+            this.bt_yes.Size = new System.Drawing.Size(75, 23);
+            this.bt_yes.TabIndex = 51;
+            this.bt_yes.Text = "确认";
+            this.bt_yes.UseVisualStyleBackColor = true;
+            this.bt_yes.Click += new System.EventHandler(this.bt_yes_Click);
             // 
             // tb_manager
             // 
-            this.tb_manager.Location = new System.Drawing.Point(469, 337);
+            this.tb_manager.Location = new System.Drawing.Point(529, 343);
             this.tb_manager.Name = "tb_manager";
             this.tb_manager.Size = new System.Drawing.Size(125, 21);
             this.tb_manager.TabIndex = 50;
             // 
             // tb_checkCycle
             // 
-            this.tb_checkCycle.Location = new System.Drawing.Point(469, 176);
+            this.tb_checkCycle.Location = new System.Drawing.Point(529, 179);
             this.tb_checkCycle.Name = "tb_checkCycle";
             this.tb_checkCycle.Size = new System.Drawing.Size(125, 21);
-            this.tb_checkCycle.TabIndex = 48;
+            this.tb_checkCycle.TabIndex = 49;
             // 
             // tb_isInWareHouse
             // 
-            this.tb_isInWareHouse.Location = new System.Drawing.Point(469, 130);
+            this.tb_isInWareHouse.Location = new System.Drawing.Point(529, 133);
             this.tb_isInWareHouse.Name = "tb_isInWareHouse";
             this.tb_isInWareHouse.Size = new System.Drawing.Size(125, 21);
-            this.tb_isInWareHouse.TabIndex = 47;
+            this.tb_isInWareHouse.TabIndex = 48;
             // 
             // tb_position
             // 
-            this.tb_position.Location = new System.Drawing.Point(469, 81);
+            this.tb_position.Location = new System.Drawing.Point(529, 84);
             this.tb_position.Name = "tb_position";
             this.tb_position.Size = new System.Drawing.Size(125, 21);
-            this.tb_position.TabIndex = 46;
+            this.tb_position.TabIndex = 47;
             // 
             // tb_serialNumber
             // 
-            this.tb_serialNumber.Location = new System.Drawing.Point(160, 281);
+            this.tb_serialNumber.Location = new System.Drawing.Point(220, 284);
             this.tb_serialNumber.Name = "tb_serialNumber";
             this.tb_serialNumber.Size = new System.Drawing.Size(125, 21);
-            this.tb_serialNumber.TabIndex = 45;
+            this.tb_serialNumber.TabIndex = 46;
             // 
             // tb_manufactor
             // 
-            this.tb_manufactor.Location = new System.Drawing.Point(160, 226);
+            this.tb_manufactor.Location = new System.Drawing.Point(220, 229);
             this.tb_manufactor.Name = "tb_manufactor";
             this.tb_manufactor.Size = new System.Drawing.Size(125, 21);
-            this.tb_manufactor.TabIndex = 44;
+            this.tb_manufactor.TabIndex = 45;
             // 
             // tb_model
             // 
-            this.tb_model.Location = new System.Drawing.Point(160, 176);
+            this.tb_model.Location = new System.Drawing.Point(220, 179);
             this.tb_model.Name = "tb_model";
             this.tb_model.Size = new System.Drawing.Size(125, 21);
-            this.tb_model.TabIndex = 43;
+            this.tb_model.TabIndex = 44;
             // 
             // tb_name
             // 
-            this.tb_name.Location = new System.Drawing.Point(160, 130);
+            this.tb_name.Location = new System.Drawing.Point(220, 133);
             this.tb_name.Name = "tb_name";
             this.tb_name.Size = new System.Drawing.Size(125, 21);
-            this.tb_name.TabIndex = 42;
+            this.tb_name.TabIndex = 43;
             // 
             // tb_tagid
             // 
-            this.tb_tagid.Location = new System.Drawing.Point(160, 81);
+            this.tb_tagid.Location = new System.Drawing.Point(220, 84);
             this.tb_tagid.Name = "tb_tagid";
-            this.tb_tagid.ReadOnly = true;
             this.tb_tagid.Size = new System.Drawing.Size(125, 21);
-            this.tb_tagid.TabIndex = 41;
+            this.tb_tagid.TabIndex = 42;
             // 
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(371, 346);
+            this.label12.Location = new System.Drawing.Point(431, 349);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(47, 12);
-            this.label12.TabIndex = 40;
+            this.label12.TabIndex = 41;
             this.label12.Text = "负责人;";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(371, 235);
+            this.label11.Location = new System.Drawing.Point(431, 232);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(89, 12);
-            this.label11.TabIndex = 39;
+            this.label11.TabIndex = 40;
             this.label11.Text = "上次检验时间：";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(371, 179);
+            this.label9.Location = new System.Drawing.Point(431, 182);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(65, 12);
-            this.label9.TabIndex = 37;
+            this.label9.TabIndex = 39;
             this.label9.Text = "检定周期：";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(371, 133);
+            this.label8.Location = new System.Drawing.Point(431, 136);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(65, 12);
-            this.label8.TabIndex = 36;
+            this.label8.TabIndex = 38;
             this.label8.Text = "在库状态：";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(371, 84);
+            this.label7.Location = new System.Drawing.Point(431, 87);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(65, 12);
-            this.label7.TabIndex = 35;
+            this.label7.TabIndex = 37;
             this.label7.Text = "货架位置：";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(83, 337);
+            this.label6.Location = new System.Drawing.Point(143, 340);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(65, 12);
-            this.label6.TabIndex = 34;
+            this.label6.TabIndex = 36;
             this.label6.Text = "生产日期：";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(83, 229);
+            this.label5.Location = new System.Drawing.Point(143, 232);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(65, 12);
-            this.label5.TabIndex = 33;
+            this.label5.TabIndex = 35;
             this.label5.Text = "生产厂商：";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(83, 284);
+            this.label4.Location = new System.Drawing.Point(143, 287);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(65, 12);
-            this.label4.TabIndex = 32;
+            this.label4.TabIndex = 34;
             this.label4.Text = "出厂编号：";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(83, 179);
+            this.label3.Location = new System.Drawing.Point(143, 182);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(65, 12);
-            this.label3.TabIndex = 31;
+            this.label3.TabIndex = 33;
             this.label3.Text = "型号规格：";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(83, 133);
+            this.label2.Location = new System.Drawing.Point(143, 136);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(65, 12);
-            this.label2.TabIndex = 30;
+            this.label2.TabIndex = 32;
             this.label2.Text = "仪器名称：";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(83, 84);
+            this.label1.Location = new System.Drawing.Point(143, 87);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 12);
-            this.label1.TabIndex = 29;
-            this.label1.Text = "标签 ID:";
+            this.label1.TabIndex = 31;
+            this.label1.Text = "标签I D:";
             // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(373, 289);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(65, 12);
-            this.label10.TabIndex = 55;
-            this.label10.Text = "当前数量：";
-            // 
-            // tb_instrumentNumber
-            // 
-            this.tb_instrumentNumber.Location = new System.Drawing.Point(469, 280);
-            this.tb_instrumentNumber.Name = "tb_instrumentNumber";
-            this.tb_instrumentNumber.Size = new System.Drawing.Size(125, 21);
-            this.tb_instrumentNumber.TabIndex = 56;
-            // 
-            // time_lastCheckTimes
-            // 
-            this.time_lastCheckTimes.Location = new System.Drawing.Point(469, 226);
-            this.time_lastCheckTimes.Name = "time_lastCheckTimes";
-            this.time_lastCheckTimes.Size = new System.Drawing.Size(125, 21);
-            this.time_lastCheckTimes.TabIndex = 57;
-            // 
-            // Updata_instrument
+            // AddOrModifyInstrument
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.time_lastCheckTimes);
             this.Controls.Add(this.tb_instrumentNumber);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.time_productionDate);
-            this.Controls.Add(this.label13);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.bt_no);
+            this.Controls.Add(this.bt_yes);
             this.Controls.Add(this.tb_manager);
             this.Controls.Add(this.tb_checkCycle);
             this.Controls.Add(this.tb_isInWareHouse);
@@ -313,18 +311,22 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "Updata_instrument";
-            this.Text = "修改仪器";
+            this.Name = "AddOrModifyInstrument";
+            this.Text = "AddOrModifyInstrument";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker time_lastCheckTimes;
+        private System.Windows.Forms.TextBox tb_instrumentNumber;
+        private System.Windows.Forms.Label label10;
         private System.Windows.Forms.DateTimePicker time_productionDate;
-        private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button bt_no;
+        private System.Windows.Forms.Button bt_yes;
         private System.Windows.Forms.TextBox tb_manager;
         private System.Windows.Forms.TextBox tb_checkCycle;
         private System.Windows.Forms.TextBox tb_isInWareHouse;
@@ -345,8 +347,5 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.TextBox tb_instrumentNumber;
-        private System.Windows.Forms.DateTimePicker time_lastCheckTimes;
     }
 }
